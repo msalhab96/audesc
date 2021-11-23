b5e7b672e0c098d0f2d015a9dcacb9e844ac7b37
Mahmoud Salhab <mahmoud@salhab.work>
2021-11-23 20:01:41 +0200

    flac files are now supported

--- audesc/describers.py
+++ audesc/describers.py
@@ -1,6 +1,12 @@
+from exceptions import CorruptedFileError
 from base import AudioDescriber, Content
 from dataclasses import dataclass
-from utils import bytes_to_int, bytes_to_short
+from utils import (
+    bytes_to_int,
+    bytes_to_short,
+    mask_bytes,
+    bits_shift_right
+    )
 
 
 @dataclass
@@ -9,7 +15,23 @@ class Range:
     end: int
 
 
-class WaveDescriber(AudioDescriber):
+class BaseDescriber(AudioDescriber):
+    header = Range(None, None)
+
+    def __init__(self, file_path: str) -> None:
+        super().__init__()
+        self.file_path = file_path
+        self.content = Content(
+            file_path,
+            start=self.header.start,
+            end=self.header.end
+            )
+
+    def _get_buffer(self, slice_range: Range) -> bytes:
+        return self.content[slice_range.start: slice_range.end]
+
+
+class WaveDescriber(BaseDescriber):
     header = Range(0, 44)
     file_size = Range(4, 8)
     channels = Range(22, 24)
@@ -19,15 +41,7 @@ class WaveDescriber(AudioDescriber):
     num_samples = Range(40, 44)
 
     def __init__(self, file_path: str) -> None:
-        super().__init__()
-        self.content = Content(
-            file_path,
-            start=WaveDescriber.header.start,
-            end=WaveDescriber.header.end
-            )
-
-    def _get_buffer(self, slice_range: Range) -> bytes:
-        return self.content[slice_range.start: slice_range.end]
+        super().__init__(file_path)
 
     def get_file_size(self) -> int:
         return bytes_to_int(
@@ -60,7 +74,7 @@ class WaveDescriber(AudioDescriber):
         return int(bytes_to_short(
             self._get_buffer(WaveDescriber.sample_width),
             little_endian=True
-        )) // 8
+        ))
 
     def get_num_samples(self) -> int:
         return int(bytes_to_int(
@@ -70,3 +84,54 @@ class WaveDescriber(AudioDescriber):
 
     def get_duration(self) -> float:
         return self.get_num_samples() / self.get_sampling_rate()
+
+
+class FlacDescriber(BaseDescriber):
+    header = Range(0, 26)
+    sampling_rate = Range(18, 21)
+    max_sampling_rate = 655350
+    channels = Range(20, 21)
+    channels_mask = 0xc
+    sample_width = Range(20, 22)
+    sample_width_mask = 0x1f0
+    num_samples = Range(22, None)
+    num_samples_mask = 0xfffffffff
+
+    def __init__(self, file_path: str) -> None:
+        super().__init__(file_path)
+
+    def get_sampling_rate(self) -> int:
+        buffer = self._get_buffer(FlacDescriber.sampling_rate)
+        result = bits_shift_right(buffer, 4)
+        if result > FlacDescriber.max_sampling_rate:
+            raise CorruptedFileError(self.file_path)
+        return result
+
+    def get_num_samples(self) -> int:
+        buffer = self._get_buffer(FlacDescriber.num_samples)
+        result = mask_bytes(buffer, FlacDescriber.num_samples_mask)
+        return None if result == 0 else result
+
+    def get_duration(self) -> float:
+        num_samples = self.get_num_samples()
+        sampling_rate = self.get_sampling_rate()
+        if num_samples is None:
+            return
+        return num_samples / sampling_rate
+
+    def get_bit_rate(self):
+        return None
+
+    def get_byte_rate(self):
+        return None
+
+    def get_channels_count(self):
+        buffer = self._get_buffer(FlacDescriber.channels)
+        result = mask_bytes(buffer, FlacDescriber.channels_mask)
+        return result + 1
+
+    def get_sample_width(self):
+        buffer = self._get_buffer(FlacDescriber.sample_width)
+        result = mask_bytes(buffer, FlacDescriber.sample_width_mask)
+        result = bits_shift_right(result, 4)
+        return result + 1
