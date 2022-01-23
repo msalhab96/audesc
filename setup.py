5706d376dcfce40e38f75de332ecf90893059f53
msalhab96 <mahmoud@salhab.work>
2022-01-23 13:33:22 +0200

    setup.py file updated

--- setup.py
+++ setup.py
@@ -2,8 +2,10 @@ from setuptools import setup
 
 setup(
     name='audesc',
+    packages = ['audesc',],
+    license='MIT',
     maintainer='Mahmoud Salhab',
     description='a set of python modules to describe audios\' meta data',
     url='https://github.com/msalhab96/audesc',
-    version='0.0.1'
+    version='0.0.2'
 )
