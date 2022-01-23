e4a4b4759372025854d330217fa2d0f7c1dc371d
msalhab96 <mahmoud@salhab.work>
2022-01-23 11:22:07 +0200

    Description class updated

--- audesc/format.py
+++ audesc/format.py
@@ -1,26 +1,33 @@
 from dataclasses import dataclass
-
+from typing import Union
 
 @dataclass
 class Description:
-    duration: float
-    bit_rate: int
-    sampling_rate: int
+    duration: Union[float, None]
+    bit_rate: Union[int, None]
+    sampling_rate: Union[int, None]
+    channels_count: Union[int, None]
+    num_samples: Union[int, None]
 
     @property
     def number_of_samples(self) -> int:
         return int(self.sampling_rate * self.duration)
 
     def __str__(self):
-        return '''sr: {} sample/sec, duration: {} sec, br: {} kb/sec'''.format(
+        return """sr: {} sample/sec, duration: {} sec, br: {} kb/sec, ch: {} channels, n_s: {} samples""".format(
             self.sampling_rate,
             self.duration,
-            self.bit_rate
+            self.bit_rate,
+            self.channels_count,
+            self.num_samples
             )
 
+
     def __repr__(self):
-        return '''Description({}, {}, {})'''.format(
+        return '''Description({}, {}, {}, {}, {})'''.format(
             self.duration,
             self.bit_rate,
-            self.sampling_rate
+            self.sampling_rate,
+            self.channels_count,
+            self.num_samples
             )
